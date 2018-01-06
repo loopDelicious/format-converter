3a8b391117fe55e762760ff9b6656bc8e91be88d
Joyce Lin <joycelin79@gmail.com>
2018-01-05 16:54:54 -0800

    update comments in script file

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-// postman
 var expect = require('expect.js'),
     Swagger2Postman = require('./convert.js'),
     fs = require('fs'),
     path = require('path');
 
+// convert your Swagger 2.0 files to Postman 1.0
 function handleConversion() {
 
-    // convert your Swagger 2.0 files
     var samples = fs.readdirSync(path.join(__dirname, 'test/data'));
+
     samples.map(function (sample) {
+
+        // to access swagger files located in separate directory
         var samplePath = path.join(__dirname, 'test/data', sample);
+
+        // initialize class, and convert swagger to postman
         var swagger = require(samplePath),
-            converter = new Swagger2Postman(), // initialize the class
+            converter = new Swagger2Postman(),
             convertResult = converter.convert(swagger);
 
         // check the result of the conversion
         expect(convertResult.status).to.be('passed');
 
-        var outputFilename = './pm2-' + sample; // path of the file to output
-        fs.writeFileSync(outputFilename, JSON.stringify(convertResult.collection, null, 4)); // write to the file system
+        // write to the file system
+        var outputFilename = './pm2-' + sample;
+        fs.writeFileSync(outputFilename, JSON.stringify(convertResult.collection, null, 4));
         console.log('Saved to ' + outputFilename);
 
     });
